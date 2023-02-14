0ebcb641eaf3bd1ab9a9b4747da7df8a17df2723
Alex Dieguez <afdieguezg@correo.url.edu.gt>
2023-02-14 09:58:08 -0600

    comit de main

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 print('Alex Fernando Dieguez Guillen')
 print('No de carne. 1535823')
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
+
+    archivo = open('.gitignore')
+
+    archivo.close()
+
